e84cef0b0d240002d77140d7d55e889f2b2d42fb
Evonne Truong <evonne.truong@sjsu.edu>
2021-03-12 17:14:55 -0800

    make array

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
+// You meet an adventurer
+let adventurer = [{
+  name: "Gin-Gin Mule",
+  who: "The Thrill Seeking Singer"
+}, {
+  name: "Ozerin Everlight",
+  who: "The Charming Drifter"
+}, {
+  name: "Asphodel Nettle",
+  who: "The Ghostly Protector"
+}, {
+  name: "Helios Sterling",
+  who: "The Crafty Scholar"
+}, {
+  name: "Ura Azir-Keiran",
+  who: "The Curious Tinkerer"
+}];
+
+let randomIndex;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(200);
+
+
+
 }
 
 function draw() {
-  background(220);
+
+}
+
+function mousePressed() {
+  background( random(200,255))
+  randomIndex = int(random(adventurer.length));
+  text(adventurer[randomIndex].name, 50, 50);
+  adventurer.splice(randomIndex, 1);
 }
