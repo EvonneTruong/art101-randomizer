a89abfb47e0d72b572453f95dff3ab844f2756f3
Evonne Truong <evonne.truong@sjsu.edu>
2021-03-20 21:00:29 -0700

    hm more set up

--- sketch3.js
+++ sketch3.js
@@ -1,7 +1,7 @@
 // You look into an ornate mirror
 
 //what dnd class are you
-let class = ["Artificer", "Barbarian", "Bard", "Blood Hunter", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];
+let characlass = ["Artificer", "Barbarian", "Bard", "Blood Hunter", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];
 
 //What dnd race are you picking from; general groups only
 let race = ["Common", "Exotic", "Monstrous", "Unearthed Arcana", "Eberron", "Plane Shift", "Ravnica", "Theros"];
@@ -11,13 +11,14 @@ let characterbg = ["Acolyte", "Anthropologist", "Archaeologist", "Charlatan", "C
   "Criminal", "Entertainer", "Faceless", "Faction Agent", "Far Traveler", "Folk Hero",
   "Gladiator", "Guild Artisan", "Guild Merchant", "Haunted One", "House Agent", "Hermit", "Inheritor",
   "Investigator", "Knight", "Knight og the Order", " Marine", "Mercenary Veteran", "Noble", "Outlander",
-  "Pirate", "Sage", "Sailor", "Soldier", "Spy", "Urban Bounty Hunter", "Urchin"];
+  "Pirate", "Sage", "Sailor", "Soldier", "Spy", "Urban Bounty Hunter", "Urchin"
+];
 
 let randomIndex;
 
 function setup() {
   createCanvas(600, 600);
-  background(0);
+  background(0, 0, 0);
   textSize(32);
 
   text("click to see who", 50, 50);
@@ -29,26 +30,9 @@ function draw() {
 }
 
 function randomizer() {
-  animating = false;
 
-  if (adventurer[0]) {
-    // pulls random name and then takes it out
-    background(random(200, 255));
-    randomIndex = int(random(adventurer.length));
-    // text(adventurer[randomIndex].name + " " + adventurer[randomIndex].who , 50, 50);
-    // adventurer.splice(randomIndex, 1);
-    text(`${adventurer[randomIndex].name}
-    ${adventurer[randomIndex].who}`, 50, 50);
-    adventurer.splice(randomIndex, 1);
-  } else {
-    background(random(200, 255));
-    text("They have all gone on their way", 50, 50);
-  }
 }
 
 function mousePressed() {
-  animating = true;
-  setTimeout(randomizer, 2000);
-
 
 }
