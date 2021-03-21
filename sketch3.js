76c1392b823e84b201e3e63ded970c5f1d1d97a3
Evonne Truong <evonne.truong@sjsu.edu>
2021-03-20 22:28:45 -0700

    jesus help me

--- sketch3.js
+++ sketch3.js
@@ -16,23 +16,66 @@ let characterbg = ["Acolyte", "Anthropologist", "Archaeologist", "Charlatan", "C
 
 let randomIndex;
 
+let spark = [];
+
+function preload() {
+  frame = loadImage('frame.png')
+}
+
 function setup() {
   createCanvas(600, 600);
   background(0, 0, 0);
-  textSize(32);
+  imageMode(CENTER);
+  image(frame, 300,300);
+  textSize(24);
 
-  text("click to see who", 50, 50);
+  spark[0] = new Spark(0, 0);
+  spark[1] = new Spark(25, 200, 1.5,);
+  spark[2] = new Spark(500, -175, .5,);
+  spark[3] = new Spark(50, -175, 1.5,);
+  spark[4] = new Spark(-135, 500, .75,);
 
 }
 
 function draw() {
 
+  spark[0].display();
+  spark[1].display();
+  spark[2].display();
+  spark[3].display();
+  spark[4].display();
+}
+
+function buttonPressed(){}
+
+function sparks(){
+  fill(255);
+  noStroke();
+  beginShape();
+  vertex(80,100);
+   quadraticVertex(85, 85, 100, 80);
+    quadraticVertex(85, 75, 80, 60);
+   quadraticVertex(75, 75, 60, 80);
+  quadraticVertex(75, 85, 80,100);
+  endShape();
+
+  beginShape();
+  vertex(180,200);
+   quadraticVertex(185, 185, 200, 180);
+    quadraticVertex(185, 175, 180, 160);
+   quadraticVertex(175, 175, 160, 180);
+  quadraticVertex(175, 185, 180,200);
+  endShape();
+
+  beginShape();
+  vertex(550,160);
+   quadraticVertex(555, 145, 570, 140);
+    quadraticVertex(555, 135, 550, 120);
+   quadraticVertex(545, 135, 530, 140);
+  quadraticVertex(545, 145, 550,10);
+  endShape();
 }
 
 function randomizer() {
-
-}
-
-function mousePressed() {
 
 }
---
+++ spark.js
@@ -0,0 +1,24 @@
+class Spark {
+  constructor(_xPos, _yPos, _size) {
+    this.xPos = _xPos;
+    this.yPos = _yPos;
+    this.size = _size;
+  }
+
+  display(){
+    push();
+    scale(this.size);
+  translate (this.xPos, this.yPos)
+
+    //create the image
+    fill(255);
+    noStroke();
+    beginShape();
+    vertex(80,100);
+     quadraticVertex(85, 85, 100, 80);
+      quadraticVertex(85, 75, 80, 60);
+     quadraticVertex(75, 75, 60, 80);
+    quadraticVertex(75, 85, 80,100);
+    endShape();
+  }
+}
